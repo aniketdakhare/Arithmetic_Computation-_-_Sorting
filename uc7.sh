a33ae72ec13187ab516c524aea490ac4edb117e3
“Aniket_D” <aniketdakhare@gmail.com>
2020-05-26 11:39:30 +0530

    Readed the values from the Dictionary into the array

---
+++ uc7.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+read -p "Enter value of 'a'  " a
+read -p "Enter value of 'b'  " b
+read -p "Entar value if 'c'  " c
+
+declare -A compute
+compute[exp"0"]=$(($a+$b*$c))
+compute[exp"1"]=$(($a*$b+$c))
+compute[exp"2"]=$(($c+$a/$b))
+compute[exp"3"]=$(($a%$b+$c))
+
+i=0
+for key in ${!compute[@]}
+do
+	arr[$i]=${compute[$key]}
+	((i++))
+done
+echo Computation result in Array ${arr[@]}
